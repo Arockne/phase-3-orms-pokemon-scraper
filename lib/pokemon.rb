b0776abda175cff948dd9dce925887c950eb6b33
Daniel Calvin <arockne2003@yahoo.com>
2021-10-14 09:32:34 -0700

    commit tree d0a316211c1a1e723152cdd5a5a44c8cb9a221db

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,30 @@
+require 'pry'
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+
+  def initialize(id: nil, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+
+  def self.save(name, type, db)
+    sql = <<-SQL
+    INSERT INTO pokemon (name, type)
+    VALUES (?,?)
+    SQL
+    
+    db.execute(sql, name, type)
+  end
+
+  def self.find(id, db)
+    sql = <<-SQL
+      SELECT *
+      FROM pokemon
+      WHERE id = ?
+    SQL
+    match = db.execute(sql, id)[0]
+    self.new(id: match[0], name: match[1], type: match[2], db: db)
+  end
 end
